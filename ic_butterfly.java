22cf71c0e4e57dc5b770ef93615576e53e4c16ec
avngbalaji <avng.balaji@gmail.com>
2025-01-29 00:10:48 +0530

    hill, diamond, butterfly columns

---
+++ ic_butterfly.java
@@ -0,0 +1,50 @@
+class ic_butterfly
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		
+		for(int i=1; i<n; i++)
+		{
+			int p=1,q=1;
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(p++ +" ");
+			}
+			for(int j=i; j<n; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i; j<n; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(q++ +" ");
+			}
+			System.out.println();
+		}
+		for(int i=1; i<=n; i++)
+		{
+			int p=1,q=1;
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print(p++ +" ");
+			}
+			for(int j=1; j<i; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1; j<i; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print(q++ +" ");
+			}
+			System.out.println();
+		}
+	}
+}
