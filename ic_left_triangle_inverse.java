9ddd320e03477445fefd08bab96f2f207d79b59d
avngbalaji <avng.balaji@gmail.com>
2025-01-29 00:09:52 +0530

    increasing columns

---
+++ ic_left_triangle_inverse.java
@@ -0,0 +1,21 @@
+class ic_left_triangle_inverse
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		
+		for(int i=1; i<=n; i++)
+		{
+			int p=1;
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print(p++ +" ");
+			}
+			System.out.println();
+		}
+	}
+}
