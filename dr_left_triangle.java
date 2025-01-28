3c81f335c0f3ccb9c4e8f7ab924beae128b39ac9
avngbalaji <avng.balaji@gmail.com>
2025-01-29 00:05:01 +0530

    decreasing rows left triangles

---
+++ dr_left_triangle.java
@@ -0,0 +1,20 @@
+class dr_left_triangle
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		
+		for(int i=1, p=5; i<=n; i++, p--)
+		{
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(p+" ");
+			}
+			System.out.println();
+		}
+	}
+}
