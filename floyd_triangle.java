c856d9217766f2469ea6ac930dd7cc4f66a7e075
avngbalaji <avng.balaji@gmail.com>
2025-01-31 23:16:14 +0530

    hill and floyd triangle

---
+++ floyd_triangle.java
@@ -0,0 +1,16 @@
+class floyd_triangle
+{
+	public static void main(String[] args)
+	{
+		int n=4,p=1;
+		
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(p++ +" ");
+			}
+			System.out.println();
+		}
+	}
+}
